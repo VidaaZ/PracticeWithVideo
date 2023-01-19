c84e81e93f7ffc2cac0480afe660d12f8410ea9c
Vida <116600986+VidaaZ@users.noreply.github.com>
2023-01-19 15:52:35 -0500

    Add files via upload

---
+++ RealProject.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using RealPractice.Models;
+
+namespace PracticeWithVideo.DataAccess.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<FoodType> FoodType { get; set; }
+        public DbSet<MenuItem> MenuItem { get; set; }
+
+
+
+    }
+}
